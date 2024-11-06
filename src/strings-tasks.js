f9a8d994696983c91ddc60feddaa3e5855db4eb9
ElderDragonfly <ElderDragonfly@yandex.ru>
2024-11-06 20:47:38 +0300

    fix: try check red strings

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -147,8 +147,8 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(str, value) {
-  return str.replace(value, '');
+function removeFirstOccurrences(/* str, value */) {
+  throw new Error('Not implemented');
 }
 
 /**
