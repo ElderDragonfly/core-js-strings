365cc4f742a71006862fcfb29d86b2a4a80a725d
ElderDragonfly <ElderDragonfly@yandex.ru>
2024-11-08 14:10:41 +0300

    feat: add more tests

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -38,7 +38,7 @@ function getStringLength(value) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-  return typeof value === 'string';
+  return typeof value === 'string' || value instanceof String;
 }
 
 /**
@@ -241,7 +241,7 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('12345') => '54321'
  */
 function reverseString(str) {
-  return str.split().reverse().join();
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -255,8 +255,8 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
@@ -271,8 +271,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -289,8 +289,9 @@ function containsSubstring(/* str, substring */) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const matches = str.match(/[aeiouyAEIOUY]/gi);
+  return matches ? matches.length : 0;
 }
 
 /**
@@ -306,8 +307,9 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const newString = str.toLowerCase().replace(/[^a-zа-я0-9]/gi, '');
+  return newString === newString.split('').reverse().join('');
 }
 
 /**
